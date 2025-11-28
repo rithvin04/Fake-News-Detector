eab6842b190865ab689b62ae0a17f9afaf2b3cc2
rthvin04 <rithvinrajdyavathi@gmail.com>
2025-11-28 12:46:13 +0530

    first commit

--- app.py
+++ app.py
@@ -1,21 +1,32 @@
 import streamlit as st
 import joblib
+
+# Load model and vectorizer
 vectorizer = joblib.load("vectorizer.jb")
-moel = joblib.load("lr_model.jb")
+model = joblib.load("lr_model.jb")
+
 st.title("FAKE NEWS DETECTOR")
-st.write("Enter a News Artical below to check whether it is Fake or  Real.")
-new_input = st.text_area("News Article:","")
+st.write("Enter a News Article below to check whether it is Fake or Real.")
+
+# Correct variable name
+news_input = st.text_area("News Article:", "")
 
 if st.button("Check News"):
+
     if news_input.strip():
+
+        # Transform input text
         transform_input = vectorizer.transform([news_input])
+
+        # Predict
         prediction = model.predict(transform_input)
-    
-    if prediction[0]==1:
-        st.sucess("The News is Real! ")
-    else: 
-        st.error("The News is Fake! ")
-else:
-    st.warning("Please enter some text to analyze.")
-    
-    
+
+        # Show result
+        if prediction[0] == 1:
+            st.success("The News is Real!")
+        else:
+            st.error("The News is Fake!")
+
+    else:
+        st.warning("Please enter some text to analyze.")
+st.write("Developed by rithvin04")
